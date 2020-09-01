70d833f489c15e8e77b877c73ca31b80df9b9977
gang <2816102915@qq.com>
2020-09-01 19:38:32 +0800

    线程池

---
+++ src/main/java/com/example/demo/config/ExecutorServiceConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class ExecutorServiceConfiguration {
+
+    private final int coreThreadNum = 10;
+    private final int maxnThreadNum = 100;
+
+
+    @Bean(name = "")
+    public ExecutorService initSystemExecutor(){
+        //ThreadPoolExecutor executor = new ThreadPoolExecutor(coreThreadNum,maxnThreadNum,);
+        return null;
+    }
+
+
+
+
+}
